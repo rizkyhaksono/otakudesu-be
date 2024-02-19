da529921beb844bd77f65b5d1ab138bbf3436faf
rizkyhaksono <mrizkyhaksono@gmail.com>
2024-02-19 20:40:06 +0700

    feat (anime): split the episodes

--- src/app/api/anime/[slug]/episodes/[episode]/route.ts
+++ src/app/api/anime/[slug]/episodes/[episode]/route.ts
@@ -1,16 +1,13 @@
 import { NextResponse, NextRequest } from "next/server";
-import { useParams, usePathname } from "next/navigation";
 import otakudesu from "@/otakudesu";
 
 export async function GET(request: NextRequest, { params }: { params: { anime_slug: string, episode: number } }) {
-  const pathname = usePathname()
-
   try {
-    console.log(pathname)
-    console.log(useParams)
+    const urlParts = request.url.split('/');
+    const animeSlug = urlParts[5];
+    console.log(animeSlug);
 
-    console.log(params.anime_slug)
-    const data = await otakudesu.episode({ animeSlug: "kuzu-honkai-subtitle-indonesia", episodeNumber: params.episode });
+    const data = await otakudesu.episode({ animeSlug: animeSlug, episodeNumber: params.episode });
     return NextResponse.json({ data: data }, { status: 201 })
   } catch (error) {
     return NextResponse.json({ error: "Internal Server Error" }, { status: 500 })
--- src/app/api/anime/[slug]/route.ts
+++ src/app/api/anime/[slug]/route.ts
@@ -4,7 +4,6 @@ import otakudesu from "@/otakudesu";
 export async function GET(request: NextRequest, { params }: { params: { slug: string } }) {
   try {
     const data = await otakudesu.anime(params.slug);
-    console.log(data)
     return NextResponse.json({ data: data }, { status: 201 })
   } catch (error) {
     return NextResponse.json({ error: "Internal Server Error" }, { status: 500 })
