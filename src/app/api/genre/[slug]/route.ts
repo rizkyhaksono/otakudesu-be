f72a87770722f67401e1b9ab3a4435de96d88cd7
rizkyhaksono <mrizkyhaksono@gmail.com>
2024-02-20 15:53:47 +0700

    feat (cors): nextrequest cors

--- src/app/api/genre/[slug]/route.ts
+++ src/app/api/genre/[slug]/route.ts
@@ -3,12 +3,14 @@ import otakudesu from "@/otakudesu";
 
 export async function GET(request: NextRequest, { params }: { params: { slug: string, page: number } }) {
   try {
+    const newHeaders = new Headers(request.headers)
+    newHeaders.set('Access-Control-Allow-Origin', '*')
     const { searchParams } = new URL(request.url);
     const reqPage = (searchParams.get("page"));
 
     const pageNumber = reqPage ? parseInt(reqPage, 10) : 1;
     const data = await otakudesu.animeByGenre(params.slug, pageNumber);
-    return NextResponse.json({ data: data }, { status: 201 })
+    return NextResponse.json({ data: data }, { status: 201 },)
   } catch (error) {
     return NextResponse.json({ error: "Internal Server Error" }, { status: 500 })
   }
