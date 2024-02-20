4c3631bea991b9953646d9fcb0969cd7b9371452
rizkyhaksono <mrizkyhaksono@gmail.com>
2024-02-20 15:02:18 +0700

    feat (cors): set cors in genre slug

--- src/app/api/genre/[slug]/route.ts
+++ src/app/api/genre/[slug]/route.ts
@@ -1,11 +1,14 @@
 import { NextResponse, NextRequest } from "next/server";
 import otakudesu from "@/otakudesu";
+import { runMiddleware, cors } from "@/config/middleware";
 
-export async function GET(request: NextRequest, { params }: { params: { slug: string, page: number } }) {
+export async function GET(request: NextRequest, response: NextResponse, { params }: { params: { slug: string, page: number } }) {
   try {
+    await runMiddleware(request, response, cors)
+
     const { searchParams } = new URL(request.url);
     const reqPage = (searchParams.get("page"));
-    
+
     const pageNumber = reqPage ? parseInt(reqPage, 10) : 1;
     const data = await otakudesu.animeByGenre(params.slug, pageNumber);
     return NextResponse.json({ data: data }, { status: 201 })
---
+++ src/config/middleware.js
@@ -0,0 +1,18 @@
+import Cors from "cors";
+
+const cors = Cors({
+  methods: ["GET", "HEAD"],
+});
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
+
+export { cors, runMiddleware };
