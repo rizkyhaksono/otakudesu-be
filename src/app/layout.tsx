4c8b741ce7c144f8d019097c627b55c95dbe3969
rizkyhaksono <mrizkyhaksono@gmail.com>
2024-02-27 08:09:02 +0700

    feat (vercel): set cron job every endpoint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import { siteConfig } from "@/config/site";
-import "./globals.css";
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import { siteConfig } from "@/config/site"
+import { Analytics } from "@vercel/analytics/react"
+import "./globals.css"
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: {
@@ -14,7 +15,7 @@ export const metadata: Metadata = {
   icons: {
     icon: "/favicon.ico",
   },
-};
+}
 
 export const viewport = {
   themeColor: [
@@ -24,12 +25,13 @@ export const viewport = {
   icons: {
     icon: "/favicon.ico",
   },
-};
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <Analytics />
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
