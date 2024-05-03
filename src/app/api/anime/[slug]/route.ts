44b67a667a8caf54afa95e0b4cede72fbbe7e372
rizkyhaksono <mrizkyhaksono@gmail.com>
2024-05-03 10:42:20 +0700

    feat (anime): handle cors

--- src/app/api/anime/[slug]/route.ts
+++ src/app/api/anime/[slug]/route.ts
@@ -1,9 +1,9 @@
-import { NextResponse, NextRequest } from "next/server";
-import otakudesu from "@/otakudesu";
+import { NextResponse, NextRequest } from "next/server"
+import otakudesu from "@/otakudesu"
 
 export async function GET(request: NextRequest, { params }: { params: { slug: string } }) {
   try {
-    const data = await otakudesu.anime(params.slug);
+    const data = await otakudesu.anime(params.slug)
     return NextResponse.json({ data: data }, { status: 201 })
   } catch (error) {
     return NextResponse.json({ error: "Internal Server Error" }, { status: 500 })
--- src/app/api/anime/route.ts
+++ src/app/api/anime/route.ts
@@ -1,9 +1,19 @@
-import { NextResponse, NextRequest } from "next/server";
+import { NextResponse, NextRequest } from "next/server"
 
 export async function GET() {
   try {
-    return NextResponse.json({data: "You need to add [:slug]"}, {status: 201})
+    return NextResponse.json(
+      { data: "You need to add [:slug]" },
+      {
+        status: 404,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+          "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        },
+      }
+    )
   } catch (error) {
-    return NextResponse.json({error: "Internal Server Error"}, {status: 500})
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 })
   }
 }
