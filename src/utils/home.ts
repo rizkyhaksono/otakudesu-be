4b948f5eae80f58e5f6cba14dc342608e3b97e51
rizkyhaksono <mrizkyhaksono@gmail.com>
2024-09-14 23:09:15 +0700

    DO NOT MERGE v3

--- src/utils/home.ts
+++ src/utils/home.ts
@@ -6,13 +6,7 @@ import { ongoingAnime as ongoingAnimeType, completeAnime as completeAnimeType } 
 
 const { BASEURL } = process.env;
 const home = async (): Promise<{ ongoing_anime: ongoingAnimeType[], complete_anime: completeAnimeType[] }> => {
-  const { data } = await axios.get(BASEURL as string, {
-    headers: {
-      "Content-Type": "application/json",
-      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
-      "Origin": ["https://otakudesu.natee.my.id", "https://otakudesu.cloud"],
-    }
-  });
+  const { data } = await axios.get(BASEURL as string);
   const $ = load(data);
   const ongoingAnimeEls = $('.venutama .rseries .rapi:first .venz ul li').toString();
   const completeAnimeEls = $('.venutama .rseries .rapi:last .venz ul li').toString();
