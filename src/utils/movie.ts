964383cc01b786f906bdce85590837c4864ad975
rizkyhaksono <mrizkyhaksono@gmail.com>
2025-01-16 11:35:22 +0700

    fix(movie): ifraae and download links

--- src/utils/movie.ts
+++ src/utils/movie.ts
@@ -3,21 +3,28 @@ import { load } from "cheerio";
 
 const { BASEURL } = process.env;
 const movie = async (slug: string): Promise<any> => {
-  const { data } = await axios.get(`${BASEURL}/episode/${slug}`);
+  const { data } = await axios.get(`${BASEURL}episode/${slug}`);
   const $ = load(data);
 
-  const title = $(".posttl").text().trim();
-  const iframeSrc = $("#pembed iframe").attr("src");
+  const checkUrl = $(".episodelist ul li span a").attr("href");
+  const fixedUrl = checkUrl?.split("/")[4];
+
+  const { data: movieData } = await axios.get(`${BASEURL}episode/${fixedUrl}`);
+  const $$ = load(movieData);
+
+  const title = $(".posttl").html()
+  const iframeSrc = $("iframe").attr("src");
   const downloadLinks: { quality: string; links: { name: string; url: string }[] }[] = [];
+
   $(".yondarkness-title").each((index, element) => {
-    const quality = $(element).text().trim();
+    const quality = $$(element).text().trim();
     const links: { name: string; url: string }[] = [];
-    $(element)
+    $$(element)
       .next(".yondarkness-item")
       .find("a")
       .each((i, el) => {
-        const name = $(el).text().trim(); 
-        const url = $(el).attr("href") ?? ""; 
+        const name = $$(el).text().trim();
+        const url = $$(el).attr("href") ?? "";
         links.push({ name, url });
       });
     downloadLinks.push({ quality, links });
